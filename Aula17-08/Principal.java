5d53c14d031ec13dd765272f43115e13a382f75f
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2026-08-18 00:56:58 -0300

    Create Principal.java

---
+++ Aula17-08/Principal.java
@@ -0,0 +1,17 @@
+public class Principal{
+
+    public static void main(String[] args) {
+    
+    Cliente c1 = new Cliente("Duda", "Maria@duda.com");
+    Produto p1 = new Produto("Telefone", 2500);
+    Pedido o1 = new Pedido(c1.nome, p1.nome,p1.preco, 5);
+
+    Cliente c2 = new Cliente("Thiago", "Thiago@gov.br");
+    Produto p2 = new Produto("Televisão", 3000);
+    Pedido o2 = new Pedido(c2.nome, p2.nome, p2.preco, 2);
+
+    o1.resumo();
+    o2.resumo();
+
+    }
+}
